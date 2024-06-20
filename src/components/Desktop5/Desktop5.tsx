ed17e8266f0b3f5fbf9260b90bd97d4d8eee33ed
dev patel <dev291203patel@gmail.com>
2024-06-20 10:18:25 +0530

    initial commit

---
+++ src/components/Desktop5/Desktop5.tsx
@@ -0,0 +1,228 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './Desktop5.module.css';
+import { Ellipse1Icon } from './Ellipse1Icon.js';
+import { Ellipse2Icon } from './Ellipse2Icon.js';
+import { Ellipse3Icon } from './Ellipse3Icon.js';
+import { Ellipse4Icon } from './Ellipse4Icon.js';
+import { Ellipse5Icon } from './Ellipse5Icon.js';
+import { Ellipse6Icon } from './Ellipse6Icon.js';
+import { G10Icon2 } from './G10Icon2.js';
+import { G10Icon } from './G10Icon.js';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 0:3 */
+export const Desktop5: FC<Props> = memo(function Desktop5(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.image1}></div>
+      <div className={classes.farmFlexProvidesFarmersAndAgri}>
+        <div className={classes.textBlock}>Farm flex provides farmers and agricultural </div>
+        <div className={classes.textBlock2}>stakeholders withconvenient online tools and </div>
+        <div className={classes.textBlock3}>educational resources.</div>
+      </div>
+      <div className={classes.discoverANovelApproachToAgricu}>
+        <div className={classes.textBlock4}>Discover a novel approach to</div>
+        <div className={classes.textBlock5}>agricultural investment</div>
+      </div>
+      <div className={classes.rectangle3}></div>
+      <div className={classes.investQuickly}>Invest quickly</div>
+      <div className={classes.emergingChances}>Emerging chances</div>
+      <div className={classes.weStandAsThePremierAndSoleCrow}>
+        <div className={classes.textBlock6}>We stand as the premier and sole crowdfunding platform empowering</div>
+        <div className={classes.textBlock7}>you to contribute to financing our farmers.</div>
+      </div>
+      <div className={classes.rectangle6}></div>
+      <div className={classes.rectangle7}></div>
+      <div className={classes.rectangle8}></div>
+      <div className={classes.rectangle9}></div>
+      <div className={classes.chooseFromVariousOptionsWithin}>
+        <div className={classes.textBlock8}>
+          Choose from various options within the supply chain and engage in agribusiness
+        </div>
+        <div className={classes.textBlock9}>
+          endeavors supported not only by Farm Flex but also by top-quality land,{' '}
+        </div>
+        <div className={classes.textBlock10}>generational expertise, innovation, and overall superior knowledge.</div>
+      </div>
+      <div className={classes.explanationOfItsFunctionality}>Explanation of its functionality</div>
+      <div className={classes.etHarumQuidemRerumFacillisEstE}>
+        <div className={classes.textBlock11}>Et harum quidem rerum facillis est et expendita</div>
+        <div className={classes.textBlock12}>distinctio. Name libero tempore,cum soluta</div>
+        <div className={classes.textBlock13}>nobis est eligendi optio cumque nihil imedit</div>
+        <div className={classes.textBlock14}>quo minus id quod maxime placeat facere</div>
+      </div>
+      <div className={classes.chooseYourFarmshareAndFillOutT}>
+        <div className={classes.textBlock15}>Choose your farmshare </div>
+        <div className={classes.textBlock16}>and fill out the </div>
+        <div className={classes.textBlock17}>reservation form.</div>
+      </div>
+      <div className={classes.ellipse1}>
+        <Ellipse1Icon className={classes.icon} />
+      </div>
+      <div className={classes.ellipse2}>
+        <Ellipse2Icon className={classes.icon2} />
+      </div>
+      <div className={classes.ellipse3}>
+        <Ellipse3Icon className={classes.icon3} />
+      </div>
+      <div className={classes.ellipse4}>
+        <Ellipse4Icon className={classes.icon4} />
+      </div>
+      <div className={classes._1}>01</div>
+      <div className={classes._2}>02</div>
+      <div className={classes._3}>03</div>
+      <div className={classes._4}>04</div>
+      <div className={classes.rectangle10}></div>
+      <div className={classes.rectangle11}></div>
+      <div className={classes.rectangle12}></div>
+      <div className={classes.investAtYourConvenience}>Invest at your convenience</div>
+      <div className={classes.autemVelEumIureReprehenderitQu}>
+        <div className={classes.textBlock18}>
+          Autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+        </div>
+        <div className={classes.textBlock19}>
+          molestiae consequatur , vel illum qui dolorem eum fugiat quo voluptas nulla
+        </div>
+      </div>
+      <div className={classes.investorsSeemToBeDiscussingFar}>
+        <div className={classes.textBlock20}>Investors seem to be </div>
+        <div className={classes.textBlock21}>discussing FarmFlex</div>
+      </div>
+      <div className={classes.rectangle13}></div>
+      <div className={classes.telecommunicationsSpecialist}>
+        <div className={classes.textBlock22}>Telecommunications </div>
+        <div className={classes.textBlock23}>Specialist</div>
+      </div>
+      <div className={classes.javierGomez}>Javier Gomez</div>
+      <div className={classes.quisAutemVelEumIureReprehender}>
+        <div className={classes.textBlock24}>Quis autem vel eum iure</div>
+        <div className={classes.textBlock25}>reprehenderit qui in ea voluptate</div>
+        <div className={classes.textBlock26}>velit esse quam nihil molestiae</div>
+      </div>
+      <div className={classes.ellipse5}>
+        <Ellipse5Icon className={classes.icon5} />
+      </div>
+      <div className={classes.rectangle14}></div>
+      <div className={classes.sergioMorales}>Sergio Morales</div>
+      <div className={classes.managerOfAgroGofundAssociation}>Manager Of AgroGofund Association</div>
+      <div className={classes.atVeroEosEtAccusamusEtIustoOdi}>
+        <div className={classes.textBlock27}>At vero eos et accusamus et iusto </div>
+        <div className={classes.textBlock28}>odio dignissimos ducimus qui </div>
+        <div className={classes.textBlock29}>blanditiis praesentium</div>
+      </div>
+      <div className={classes.ellipse6}>
+        <Ellipse6Icon className={classes.icon6} />
+      </div>
+      <div className={classes.icons8RightButton641}></div>
+      <div className={classes.icons8RightButton6412}></div>
+      <div className={classes.rectangle15}></div>
+      <div className={classes.theFutureOfFarmInvestingIsFarm}>The Future of farm investing is farmflex</div>
+      <div className={classes.rectangle16}></div>
+      <div className={classes.investNow}>Invest Now</div>
+      <div className={classes.EmbracingInnovationInAgricultu}>
+        <div className={classes.textBlock30}>&quot;Embracing Innovation in Agriculture FarmFlex Revolutionizes </div>
+        <div className={classes.textBlock31}>Farm Investing for a Sustainable Future&quot;.</div>
+      </div>
+      <div className={classes.rectangle17}></div>
+      <div className={classes.rectangle18}></div>
+      <div className={classes.blogFarmFlex}>Blog farmFlex</div>
+      <div className={classes.writeEmailToUs}>write email to us </div>
+      <div className={classes.infoFarmflexCom}>info@farmflex.com</div>
+      <div className={classes.rectangle2}></div>
+      <div className={classes.signUp}>Sign Up</div>
+      <div className={classes.icons8User481}></div>
+      <div className={classes.privacy}>Privacy</div>
+      <div className={classes.terms}>terms</div>
+      <div className={classes.security}>security</div>
+      <div className={classes.lEGAL}>lEGAL</div>
+      <div className={classes.features}>features</div>
+      <div className={classes.howItWorks}>how it works</div>
+      <div className={classes.pricing}>pricing</div>
+      <div className={classes.login}>login</div>
+      <div className={classes.invest}>invest</div>
+      <div className={classes.aboutUs}>about us</div>
+      <div className={classes.team}>team</div>
+      <div className={classes.careers}>careers</div>
+      <div className={classes.contact}>contact</div>
+      <div className={classes.company}>Company</div>
+      <div className={classes.farmFlex}>FarmFlex</div>
+      <div className={classes.g10}>
+        <G10Icon className={classes.icon7} />
+      </div>
+      <div className={classes.copyright2024FarmflexPvtLTD}>Copyright 2024.Farmflex Pvt LTD</div>
+      <div className={classes.icons8Copyright501}></div>
+      <div className={classes.icons8Facebook301}></div>
+      <div className={classes.icons8Linkedin301}></div>
+      <div className={classes.icons8Instagram301}></div>
+      <div className={classes.icons8Twitter301}></div>
+      <div className={classes.investInFarmThatWillBenreadyFo}>
+        <div className={classes.textBlock32}>Invest in farm that will benready</div>
+        <div className={classes.textBlock33}>for harvest in 3-18 months </div>
+      </div>
+      <div className={classes.quickProfitInvestment}>
+        <div className={classes.textBlock34}>Quick profit </div>
+        <div className={classes.textBlock35}>investment</div>
+      </div>
+      <div className={classes.rectangle20}></div>
+      <div className={classes.browseFarm}>Browse farm</div>
+      <div className={classes.considerFarmsThatHaveLongTermI}>
+        <div className={classes.textBlock36}>Consider farms that have long</div>
+        <div className={classes.textBlock37}>term investment program.</div>
+      </div>
+      <div className={classes.longTermGrowthInvestment}>
+        <div className={classes.textBlock38}>Long-term growth</div>
+        <div className={classes.textBlock39}>investment</div>
+      </div>
+      <div className={classes.rectangle21}></div>
+      <div className={classes.learnMore}>Learn more</div>
+      <div className={classes.farmFlex2}>
+        <p className={classes.labelWrapper}>
+          <span className={classes.label}>Farm</span>
+          <span className={classes.label2}>Flex</span>
+        </p>
+      </div>
+      <div className={classes.g102}>
+        <G10Icon2 className={classes.icon8} />
+      </div>
+      <div className={classes.contact2}>Contact</div>
+      <div className={classes.blog}>Blog</div>
+      <div className={classes.product}>Product</div>
+      <div className={classes.home}>Home</div>
+      <div className={classes.team2}>Team</div>
+      <div className={classes.rectangle22}></div>
+      <div className={classes.signUp2}>Sign Up</div>
+      <div className={classes.icons8User4812}></div>
+      <div className={classes.rectangle4}></div>
+      <div className={classes.atVeroEosEtAccusamusEtIustoOdi2}>
+        <div className={classes.textBlock40}>At vero eos et accusamus et</div>
+        <div className={classes.textBlock41}>iusto odio dignissimos ducimus</div>
+        <div className={classes.textBlock42}>qui blanditiis praesentium </div>
+        <div className={classes.textBlock43}>voluptatum deleniti atque</div>
+      </div>
+      <div className={classes.investmentWithAFocusOnSocialIm}>
+        <div className={classes.textBlock44}>Investment with a focus</div>
+        <div className={classes.textBlock45}>on social impact</div>
+      </div>
+      <div className={classes.icons8FinancialGrowth641}></div>
+      <div className={classes.rectangle42}></div>
+      <div className={classes.establishAConnectionWithOurAgr}>
+        <div className={classes.textBlock46}>Establish a connection with</div>
+        <div className={classes.textBlock47}>our agricultural producers.</div>
+      </div>
+      <div className={classes.nemoEnimIpsamVoluptatemQuiaVol}>
+        nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+      </div>
+      <div className={classes.icons8Farmer641}></div>
+      <div className={classes.icons8Farmer601}></div>
+      <div className={classes.icons8RightButton6413}></div>
+      <div className={classes.icons8RightButton6414}></div>
+      <div className={classes.rectangle19}></div>
+      <div className={classes.rectangle222}></div>
+    </div>
+  );
+});
---
+++ src/components/Desktop5/Ellipse3Icon.tsx
@@ -0,0 +1,10 @@
+import { memo, SVGProps } from 'react';
+
+const Ellipse3Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 50 50' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <circle cx={25} cy={25} r={25} stroke='white' />
+  </svg>
+);
+
+const Memo = memo(Ellipse3Icon);
+export { Memo as Ellipse3Icon };
